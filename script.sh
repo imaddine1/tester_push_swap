a647e33cce016da2a6e1fb1ac0a21e4cb68e1626
imaddine1 <hh.imad2016@gmail.com>
2022-03-02 17:09:38 +0100

    add limit number

--- script.sh
+++ script.sh
@@ -3,11 +3,19 @@
 done=OK
 Error="\e[33mbash script.sh 100 10"
 MAX=0
+found=`find . -name push_swap | wc -l`
+bleu="\e[34m"
 
-
-if [ $# -lt 2 ] || [ $# -gt 2 ]
+if [ $found -eq 0 ]
 then
-	echo -n " JUST 2 ARGUMENT "
+	echo " "
+	echo -e "      ${bleu}PUT YOUR OBJECTS FILES INSIDE push_swap    "
+	echo " "
+	exit 1
+fi
+if [ $# -lt 3 ] || [ $# -gt 3 ]
+then
+	echo -n " KEEP IT 3 ARGUMENT "
 	echo -e $Error
 	exit 1
 fi
@@ -15,13 +23,13 @@ for ((i=1; i <= $2; i++))
 do
 	ARG=`bash rand.sh $1`
 	check=$(./push_swap $ARG | ./checker $ARG | tail -1)
-	if [ $check != $done ]
+	wc=`./push_swap $ARG | wc -l`
+	if [ $check != $done ] || [ $wc -gt $3 ]
 	then
 		echo "you have problem with this stack !!"
 		echo $ARG
 		exit 1
 	else
-		wc=`./push_swap $ARG | wc -l`
 		echo "$i:$wc"
 		if [ $wc -gt $MAX ]
 		then
