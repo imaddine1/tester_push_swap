71e4ee259866bf1770b210d33c228aa115a7f6a7
hanma <59666605+imaddine1@users.noreply.github.com>
2022-03-02 13:20:45 +0100

    Update script.sh
    define max instruction

--- script.sh
+++ script.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 done=OK
+Error="\e[33mbash script.sh 100 10"
+MAX=0
+
+
+if [ $# -lt 2 ] || [ $# -gt 2 ]
+then
+	echo -n " JUST 2 ARGUMENT "
+	echo -e $Error
+	exit 1
+fi
 for ((i=1; i <= $2; i++))
 do
 	ARG=`bash rand.sh $1`
@@ -12,6 +22,12 @@ do
 		exit 1
 	else
 		wc=`./push_swap $ARG | wc -l`
-		echo "$i:$wc ---------- OK"
+		echo "$i:$wc"
+		if [ $wc -gt $MAX ]
+		then
+			MAX=$wc
+		fi
 	fi
 done
+MSG="\e[92m$MAX"
+echo -e "THE MAXIMUM INSTRUCTION IS : $MSG"
