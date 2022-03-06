800ab3300dd230b2ba2db421c78a6b1f6e9b3f83
hanma <59666605+imaddine1@users.noreply.github.com>
2022-03-06 13:05:58 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
 
 done=OK
-Error="\e[33mbash script.sh 100 10"
 MAX=0
 found=`find . -name push_swap | wc -l`
-bleu="\e[34m"
+bleu="\033[1;34m"
 
 if [ $found -eq 0 ]
 then
 	echo " "
-	echo -e "      ${bleu}PUT YOUR OBJECTS FILES INSIDE push_swap    "
+	echo -e "${bleu} PUT YOUR OBJECTS FILES INSIDE push_swap"
 	echo " "
 	exit 1
 fi
 if [ $# -lt 3 ] || [ $# -gt 3 ]
 then
 	echo -n " KEEP IT 3 ARGUMENT "
-	echo -e $Error
+	echo -e "\033[1;33m bash script.sh 100 10"
 	exit 1
 fi
 for ((i=1; i <= $2; i++))
 do
 	ARG=`bash rand.sh $1`
-	check=$(./push_swap $ARG | ./checker $ARG | tail -1)
+	check=`./push_swap $ARG | ./checker $ARG | tail -1`
 	wc=`./push_swap $ARG | wc -l`
 	if [ $check != $done ] || [ $wc -gt $3 ]
 	then
@@ -37,5 +36,4 @@ do
 		fi
 	fi
 done
-MSG="\e[92m$MAX"
-echo -e "THE MAXIMUM INSTRUCTION IS : $MSG"
+echo -e "THE MAXIMUM INSTRUCTION IS : \033[1;32m$MAX"
